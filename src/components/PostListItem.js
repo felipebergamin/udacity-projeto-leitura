f0f32f345afb6306cb67761b10cbfe3750c9968a
Felipe Bergamin <felipebergamin6@gmail.com>
2018-12-26 16:47:55 -0200

    feat: PostList and PostListItem components

---
+++ src/components/PostListItem.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Box, Heading, Text, RoutedAnchor } from 'grommet';
+import { TiChartLine, TiThumbsUp, TiThumbsDown, TiMessage } from 'react-icons/ti';
+import { connect } from 'react-redux';
+import { handleUpvotePost, handleDownvotePost } from '../actions/posts';
+
+import { formatDateTime } from '../utils';
+
+class PostListItem extends Component {
+
+  handleUpvote = postid => {
+    this.props.dispatch(handleUpvotePost(postid));
+  }
+
+  handleDownvote = postid => {
+    this.props.dispatch(handleDownvotePost(postid));
+  }
+
+  render() {
+    const { post, comments } = this.props;
+
+    return (
+      <Box animation={{ type: 'fadeIn', duration: 250 }}>
+        <Heading level={4} margin={{bottom: 'none'}}>{post.title}</Heading>
+        <Text size='xsmall'>por {post.author} em <strong>{post.category}</strong> no dia {formatDateTime(post.timestamp)}</Text>
+
+        <Box direction='row' gap='medium' pad={{top: 'small'}}>
+
+          <span>
+            <TiMessage />
+            {comments.length}
+          </span>
+
+          <span>
+            <TiChartLine />
+            {post.voteScore}
+          </span>
+
+          <TiThumbsUp onClick={() => this.handleUpvote(post.id)} />
+
+          <TiThumbsDown onClick={() => this.handleDownvote(post.id)} />
+
+          <RoutedAnchor path={`/post/${post.id}`}>
+            Ler
+          </RoutedAnchor>
+        </Box>
+      </Box>
+    );
+  }
+}
+
+function mapStateToProps({comments}, {post}) {
+  return {
+    comments: comments.filter(c => c.parentId === post.id)
+  }
+}
+
+export default connect(mapStateToProps)(PostListItem);
---
+++ src/components/PostsList.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Box, Button, Select } from 'grommet';
+import { TiPen } from 'react-icons/ti';
+import { Link } from 'react-router-dom';
+
+import PostListItem from './PostListItem';
+
+class PostsList extends Component {
+  state = {
+    orderBy: '',
+    orderOptions: [
+      'Melhores Avaliados',
+      'Piores avaliados',
+      'Mais recentes',
+      'Mais antigos',
+    ]
+  };
+
+  handleOrderChange = event => {
+    this.setState(() => ({
+      orderBy: event.value,
+      selected: event.selected,
+    }))
+  }
+
+  orderPosts(posts, order) {
+    switch (order) {
+      case 'Piores avaliados':
+        return posts.sort((p1, p2) => p1.voteScore - p2.voteScore);
+      case 'Mais recentes':
+        return posts.sort((p1, p2) => p2.timestamp - p1.timestamp);
+      case 'Mais antigos':
+        return posts.sort((p1, p2) => p1.timestamp - p2.timestamp);
+      case 'Melhores Avaliados':
+      default:
+        return posts.sort((p1, p2) => p2.voteScore - p1.voteScore);
+    }
+  }
+
+  render() {
+    const postList = this.props.category
+      ? this.props.posts.filter(post => post.category === this.props.category)
+      : this.props.posts;
+
+    const { orderBy, orderOptions } = this.state;
+
+    return (
+      <Box direction='column' pad='medium'>
+        <Box direction='row'>
+          <Select
+            placeholder='Ver primeiro'
+            multiple={false}
+            value={orderBy}
+            onChange={this.handleOrderChange}
+            options={orderOptions} />
+
+          &nbsp;
+
+          <Link to='/newpost'>
+            <Button path='/newpost' label='Novo Post' icon={<TiPen />} fill />
+          </Link>
+        </Box>
+
+        {this.orderPosts(postList, orderBy)
+          .map(post => (
+            <PostListItem key={post.id} post={post} />
+          ))}
+      </Box>
+    )
+  }
+}
+
+function mapStateToProps({ posts, categories }) {
+  return { posts, categories };
+}
+
+export default connect(mapStateToProps)(PostsList);
