8643ca819a241fb77fb8a3f08bc908100c8bb1ed
Felipe Bergamin <felipebergamin6@gmail.com>
2018-12-26 16:48:46 -0200

    chore: reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import categories from './categories';
 import posts from './posts';
 import comments from './comments';
+import editingComment from './editingComment';
 
 export default combineReducers({
   categories,
   posts,
   comments,
+  editingComment,
 });
