946c65d3922f593191894710341c5947c18eae1f
Felipe Bergamin <felipebergamin6@gmail.com>
2018-12-26 16:43:35 -0200

    chore: add upvote and downvote to post reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,7 +1,14 @@
-import { ADD_POST, RECEIVE_POSTS, REMOVE_POST, UPDATE_POST } from '../actions/posts';
+import {
+  ADD_POST,
+  RECEIVE_POSTS,
+  REMOVE_POST,
+  UPDATE_POST,
+  UPVOTE_POST,
+  DOWNVOTE_POST,
+} from '../actions/posts';
 
 export default function posts(state = [], action) {
-  switch(action.type) {
+  switch (action.type) {
     case ADD_POST:
       return [...state, action.post];
     case RECEIVE_POSTS:
@@ -9,7 +16,11 @@ export default function posts(state = [], action) {
     case REMOVE_POST:
       return state.filter(post => post.id !== action.id);
     case UPDATE_POST:
-      return state.map(post => post.id === action.post.id ? action.post : post);
+      return state.map(post => post.id === action.post.id ? {...post, ...action.post} : post);
+    case UPVOTE_POST:
+      return state.map(post => post.id === action.id ? ({ ...post, voteScore: post.voteScore + 1 }) : post);
+    case DOWNVOTE_POST:
+      return state.map(post => post.id === action.id ? ({ ...post, voteScore: post.voteScore - 1 }) : post);
     default:
       return state;
   }
