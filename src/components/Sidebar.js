e132dc8901042ab4f23d0d105ee4d78f6a14f984
Felipe Bergamin <felipebergamin6@gmail.com>
2018-12-26 16:47:32 -0200

    feat: sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Box, Heading, RoutedAnchor } from 'grommet';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <Box width='small' elevation='xlarge'>
+        <Box fill='vertical'>
+          <Box flex direction='column' tag='nav' responsive={true} className='sidebar-menu' pad='small' gap='small'>
+            <Heading level={3}>Leitura</Heading>
+
+            <RoutedAnchor path='/'>todos</RoutedAnchor>
+            {this.props.loading
+              ? <Heading level={5}>Loading...</Heading>
+              : this.props.categories.map(({name, path}) => (
+                  <RoutedAnchor path={`/posts/${path}`} key={name}>{name}</RoutedAnchor>
+              ))
+            }
+          </Box>
+        </Box>
+      </Box>
+    )
+  }
+}
+
+function mapStateToProps({ categories }) {
+  return {
+    loading: !categories,
+    categories: categories,
+  };
+}
+
+export default connect(mapStateToProps)(Sidebar);
