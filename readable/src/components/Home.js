46a24d779572d4717a82c395d2d6276546c65114
Felipe Bergamin <felipebergamin6@gmail.com>
2019-01-22 21:44:38 -0200

    refat: change Home component to a functional component

--- readable/src/components/Home.js
+++ readable/src/components/Home.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Box, Grommet } from 'grommet';
 import { grommet } from 'grommet/themes';
 import './Home.css';
 import Sidebar from './Sidebar';
 import PostsList from './PostsList';
 
-class HomeComponent extends Component {
+function HomeComponent (props) {
+  const category = props.match.params.category;
 
-  render() {
-    const category = this.props.match.params.category;
+  const myTheme = {
+    ...grommet,
+    anchor: {
+      color: 'dark-1',
+      fontWeight: 400,
+      textDecoration: 'none',
+    }
+  };
 
-    const myTheme = {
-      ...grommet,
-      anchor: {
-        color: 'dark-1',
-        fontWeight: 400,
-        textDecoration: 'none',
-      }
-    };
-
-    return (
-      <Grommet theme={myTheme} full>
-        <Box direction='column' flex fill='vertical'>
-          <Box direction='row' flex>
-            <Sidebar />
-            <Box flex>
-              <PostsList category={category} />
-            </Box>
+  return (
+    <Grommet theme={myTheme} full>
+      <Box direction='column' flex fill='vertical'>
+        <Box direction='row' flex>
+          <Sidebar />
+          <Box flex>
+            <PostsList category={category} />
           </Box>
         </Box>
-      </Grommet>
-    )
-  }
+      </Box>
+    </Grommet>
+  )
 }
 
 export default HomeComponent;
